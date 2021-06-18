7eae7b5cc0770ca8bc032d06a90b61bd11ad8b05
Aanvig <78943357+Aanvig@users.noreply.github.com>
2021-06-17 20:16:00 -0400

    Add files via upload

---
+++ C-44 Project/js/System.js
@@ -0,0 +1,17 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode, enteredCode) {
+        if(actualCode === enteredCode) 
+            return true
+        else
+            return false
+        
+    }
+
+    // Add code to authenticate the given code and the access codes.
+
+    
+
+}
